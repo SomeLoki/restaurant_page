806eb69f649cfb71a6c22ceff4a35e093e440294
someloki <tenacity686@gmail.com>
2025-06-23 00:14:53 -0700

    Feat: Add homepage

---
+++ src/home_content.js
@@ -0,0 +1,39 @@
+const homePageContent = [
+  { 
+    textContent: "Glaze Craze Donuts",
+    parent: "brand",
+    className: "brand",
+  },
+  {
+    textContent: "At Glaze Craze Donuts, we don't just make donuts we craft irresistible, melt-in-your-mouth masterpieces that elevate your sweet tooth to new heights. Our donuts are hand-rolled daily using locally sourced ingredients, infused with bold, creative flavors you won't find anywhere else. From our signature Maple Bacon Bliss to the dreamy Lavender Crème Glaze, every bite is a perfect balance of fluffy dough and decadent toppings. Why settle for ordinary when you can indulge in extraordinary?",
+    parent: "description",
+    className: "description",
+  },
+  { 
+    textContent: "Store Hours:",
+    parent: "hours",
+    className: "title",
+  },
+  { 
+    textContent: "Monday–Friday: 6:00 AM – 6:00 PM",
+    parent: "hours",
+    className: "text",
+  },
+  { 
+    textContent: "Saturday–Sunday: 7:00 AM – 4:00 PM",
+    parent: "hours",
+    className: "text",
+  },
+  { 
+    textContent: "Location:",
+    parent: "location",
+    className: "title",
+  },
+  { 
+    textContent: "123 Sprinkle Street, Sweetville, CA 90210",
+    parent: "location",
+    className: "text",
+  },
+]
+
+export { homePageContent }
--- src/index.js
+++ src/index.js
@@ -1,1 +1,43 @@
 console.log("link established");
+import { homePageContent } from "./home_content";
+console.log(homePageContent);
+
+const createDomElement = function( textContent, parentElement, ...args ) {
+  const newElement = document.createElement("div");
+  const parent = document.querySelector(parentElement);
+  if (args) {
+    newElement.classList.add(...args);
+  }
+  newElement.textContent = textContent;
+  parent.appendChild(newElement);
+}
+
+function createHomePage() {
+  for ( let content of homePageContent ) {
+    if (!checkForParent( content.parent )) {
+      createDomElement( "", "#content", content.parent, "container" )
+    }
+      createDomElement( content.textContent, `.${content.parent}`, content.className )
+  }
+}
+
+function checkForParent( parent ) {
+  console.log( document.querySelector(`.${parent}`) ? true : false )
+  return document.querySelector(`.${parent}`) ? true : false
+}
+
+createHomePage();
+
+function clearContent() {
+  const contentToClear = document.querySelectorAll(".container > *")
+  const containersToClear = document.querySelectorAll("#content > *")
+  for ( let content of contentToClear ) {
+    content.remove()
+  }
+  for ( let container of containersToClear ) {
+    container.remove()
+  }
+}
+
+export { clearContent }
+window.clearContent = clearContent
